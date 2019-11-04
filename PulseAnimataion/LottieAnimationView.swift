89eda85ebd2f9cb39104821226875a81e372874e
sadegh <sadeghitunes2@gmail.com>
2019-11-04 17:18:18 +0330

    no message

--- PulseAnimataion/LottieAnimationView.swift
+++ PulseAnimataion/LottieAnimationView.swift
@@ -9,65 +9,83 @@
 import Foundation
 import Lottie
 
-/**
 
+// animation view status
+enum AnimationStatus {
+      case play
+      case stop
+      case pause
+      case non
+}
+
+protocol LottieAnimationViewDelegate: class {
+    func didAnimationStatusChanged(status: AnimationStatus)
+}
+
+/**
 Create a view and subclasss it to LottieAnimationView then call setupAnimation method to Initalize Animation
 
- **Check This Eample :**
+ # Check This Eample :
      
  This method like Intializer And should call
- 
-     animationView.setupAnimation(animationName: "AnimationName")
-     
-
- Play animation
- 
-     animationView.playAnimation()
-    
- Pause animation
- 
-     animationView.pauseAnimation()
-     
- Stop animation
- 
-     animationView.stopAnimation()
-    
- 
+```
+animationView.setupAnimation(animationName: "AnimationName")
+```
+ + Play animation
+ ```
+animationView.playAnimation()
+```
+ + Pause animation
+```
+animationView.pauseAnimation()
+```
+ + Stop animation
+```
+animationView.stopAnimation()
+```
  */
-
 class LottieAnimationView: UIView {
     
-    private enum AnimationStatus {
-        case play
-        case stop
-        case pause
-        case non
-    }
-    
     private lazy var animationView: AnimationView = {
-        let animationView = AnimationView()
+        let animationView = AnimationView(name: "", animationCache: LRUAnimationCache.sharedCache)
         animationView.translatesAutoresizingMaskIntoConstraints = false
         animationView.contentMode = .scaleAspectFit
         animationView.clipsToBounds = false
+        
         return animationView
         
     }()
     
+    weak var lottieDelegate: LottieAnimationViewDelegate?
+    
     private var animation: Animation!
     
-    private var animationStatus: AnimationStatus = .non
+    private var didAnimationPlayed: Bool = false
+
+    /** Current status of animation */
+    private var animationStatus: AnimationStatus {
+        return .non
+    }
         
-    /// This is intialize of lottie animation view
-     func setupAnimation(animationName: String, loopMode: LottieLoopMode = .loop) {
-         
-          self.animation = Animation.named(animationName)
-          animationView.loopMode = loopMode
-          animationView.animation = self.animation
-     }
+    /** This method is like Intializer of class and you should call it when you want create animation  */
+    /// - parameter animationName: animation name in current bundle
+    /// - parameter loopMode: loop mode of animatoin default is infinity loop 
+    func setupAnimation(animationName: String, loopMode: LottieLoopMode = .loop, animationSpeed: CGFloat = 1) {
+        
+        self.animation = Animation.named(animationName)
+        animationView.loopMode = loopMode
+        animationView.animation = self.animation
+        self.animationView.animationSpeed = animationSpeed
+    }
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         addSubview(animationView)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(LottieAnimationView.willEnterForegroundNotification) , name: UIApplication.willEnterForegroundNotification, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(LottieAnimationView.didEnterBackgroundNotification) , name: UIApplication.didEnterBackgroundNotification, object: nil)
+        
     }
     
     required init?(coder: NSCoder) {
@@ -86,45 +104,55 @@ class LottieAnimationView: UIView {
         setConstraint()
         
     }
-    
+    // Play animation willEnterForegroundNotification
     @objc private func willEnterForegroundNotification() {
         print("willEnterForegroundNotification")
-        self.playAnimation()
+        if self.didAnimationPlayed {
+            self.playAnimation()
+
+        }
         
     }
-    
+    // Puase animation didEnterBackgroundNotification
     @objc private func didEnterBackgroundNotification() {
         print("didEnterBackgroundNotification")
+        self.didAnimationPlayed = true
         self.pauseAnimation()
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
     }
 }
 
 extension LottieAnimationView {
     
-    /// Play animation at time if animation is paused this method resume it
+    /** Play animation when animation is paused this method resume it */
+    /// - parameter completion: do something after play animation
     func playAnimation(completion:(() -> Void)? = nil) {
         
-        animationStatus = .play
-    
-        animationView.play { (bool) in
-            completion?()
-        }
+        lottieDelegate?.didAnimationStatusChanged(status: .play)
+        animationView.play()
+        completion?()
+
     }
-    /// Pause the animation at time
+    /** Pause animation at time */
+    /// - parameter completion: do something after pause animation
     func pauseAnimation(completion:(() -> Void)? = nil) {
         
-        animationStatus = .pause
+        lottieDelegate?.didAnimationStatusChanged(status: .pause)
         self.animationView.pause()
         completion?()
     }
-    /// Stop animation at time
+    /** Stop animation at time */
+    /// - parameter completion: do something after stop animation
     func stopAnimation(completion:(() -> Void)? = nil) {
         
-        animationStatus = .stop
-        
+        lottieDelegate?.didAnimationStatusChanged(status: .stop)
         self.animationView.stop()
         completion?()
     }
+   
 }
 
 extension LottieAnimationView {
--- PulseAnimataion/ViewController.swift
+++ PulseAnimataion/ViewController.swift
@@ -13,20 +13,24 @@ import Lottie
 class ViewController: UIViewController {
 
     @IBOutlet weak var animationView: LottieAnimationView!
-        
+    
+    @IBOutlet weak var imageview: UIImageView!
+    
     @IBOutlet weak var frontView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        animationView.setupAnimation(animationName:"circle")
+        imageview.image = UIImage(named: "map")
+        imageview.layer.cornerRadius = 100
+        imageview.contentMode = .scaleAspectFill
+        animationView.setupAnimation(animationName: <#T##String#>, loopMode: <#T##LottieLoopMode#>, animationSpeed: <#T##CGFloat#>)
         frontView.layer.cornerRadius = 100
-        
+        animationView.lottieDelegate = self
     }
 
+    
     @IBAction func play(_ sender: Any) {
         animationView.playAnimation()
-        
         
     }
     
@@ -38,6 +42,14 @@ class ViewController: UIViewController {
     @IBAction func stop(_ sender: Any) {
         animationView.stopAnimation()
        
+    }
+}
+extension ViewController: LottieAnimationViewDelegate {
+    
+    func didAnimationStatusChanged(status: AnimationStatus) {
+        if status == .play {
+            print("Wow im playing")
+        }
     }
 }
 
